a7c94b0b044d6867e8ab89297040878a334ccf3e
Dimitar <dimitar.mandarliev@gmail.com>
2023-10-13 23:00:15 +0300

    Added the Header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import Header from "@/components/Header"
+
 export default function Home() {
   return (
     <main>
-      <h1>Trello 2.0 AI Clone</h1>
+      {/* Header */}
+      <Header/>
+
+      {/* Board */}
+
+      <h1>Trello 2.0</h1>
     </main>
   )
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'upload.wikimedia.org',
+            port: '',
+            pathname: '/wikipedia/en/thumb/8/8c/Trello_logo.svg/1280px-Trello_logo.svg.png',
+          },
+        ],
+      }
+}
 
 module.exports = nextConfig
